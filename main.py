aad131511ea91487f6943fc98df39aee0b495d1f
mohammad <mohammadhoseinajorloo76@gmail.com>
2024-09-21 12:19:45 +0330

    style(api): add templates
    config fastapi for render home html page in home api androuting api by apirouting and add appversion in constans.
    	- add appverson constans
    	- add routing api
    	- set version app
    	- config route api in app

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, APIRouter, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse
 
 
 APPNAME = "Terminal"
 APPDESCRIPTION="Blog Website"
+APPVERSION="V1.0"
 
 
-app = FastAPI(
-        title=APPNAME,
-        description=APPDESCRIPTION,
-    )
+templates = Jinja2Templates(directory="templates")
 
 
-@app.get("/")
-async def home():
-    return {"message":"This is home page"}
+router = APIRouter()
+
+
+@router.get("/")
+async def serve_home(request: Request):
+    return templates.TemplateResponse("index.html", context={"request":request})
+
+
+app = FastAPI(title=APPNAME, description=APPDESCRIPTION, version=APPVERSION)
+app.include_router(router)
+app.mount("/static", StaticFiles(directory="/"), name="static")
